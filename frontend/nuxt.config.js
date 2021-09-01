a58437ae04119a2a1d3878c3597d8bc1e30cfe3b
DasPeTe <daspetemail@gmail.com>
2021-09-01 20:29:18 +0200

    feat: init

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,7 @@
+export default {
+    components: true,
+    buildModules: ['@nuxtjs/tailwindcss'],
+    tailwindcss: {
+        // jit: true
+    }
+}
---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page text-white font-bold text-4xl px-8 py-4 bg-yellow-700 inline-block">Greetz</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+body {
+    @apply bg-red-900;
+}
+</style>
